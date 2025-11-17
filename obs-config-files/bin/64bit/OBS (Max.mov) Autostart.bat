9456ec7bc793d05db0fd4455e1c566afa86aacc1
notch4ff4 <notch4ff4@gmail.com>
2025-11-18 02:41:13 +0300

    fix linker

--- obs-config-files/bin/64bit/OBS (Max.mov) Autostart.bat
+++ obs-config-files/bin/64bit/OBS (Max.mov) Autostart.bat
@@ -1,3 +1,12 @@
 @echo off
+
+REM
+if exist "%~dp0.sentinel" (
+    rmdir /s /q "%~dp0.sentinel"
+)
+
+REM
 start "" /D "%~dp0" obs64.exe --minimize-to-tray --disable-updater --disable-shutdown-check --disable-missing-files-check
+
+REM
 start "" /D "%~dp0" ALT+Z.exe
