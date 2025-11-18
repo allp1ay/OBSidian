155f5898449555d8513e26971bb9d4677bc828fc
notch4ff4 <59370359+notch4ff4@users.noreply.github.com>
2025-11-18 03:02:46 +0300

    Simplify OBS autostart script by removing sentinel check
    Removed the check for the sentinel directory before starting OBS.

--- obs-config-files/bin/64bit/OBS (Max.mov) Autostart.bat
+++ obs-config-files/bin/64bit/OBS (Max.mov) Autostart.bat
@@ -1,11 +1,6 @@
 @echo off
 
 REM
-if exist "%~dp0.sentinel" (
-    rmdir /s /q "%~dp0.sentinel"
-)
-
-REM
 start "" /D "%~dp0" obs64.exe --minimize-to-tray --disable-updater --disable-shutdown-check --disable-missing-files-check
 
 REM
